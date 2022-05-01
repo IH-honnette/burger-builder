7c73a97bff05e4134972aaee318909e74142a604
IH-honnette <ihozohonnette12@gmail.com>
2022-05-01 20:13:23 +0200

    changed the layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/ToolBar/ToolBar'
 
 const layout =(props) =>(
     <Aux>
-   <div>Toolbar, SideBar, Backdrop</div>
+   {/* <div>Toolbar, SideBar, Backdrop</div> */}
+   <Toolbar/>
    <main className={classes.Content}>
        {props.children}
    </main>

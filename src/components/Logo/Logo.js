5b491abee503680f9f80fa74ca89eb6fa290dc62
IH-honnette <ihozohonnette12@gmail.com>
2022-05-02 21:35:14 +0200

    Styling the logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import  burgerLog from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const logo =(props)=>(
+       <div className={classes.Logo}>
+           <img src={burgerLog} alt="burger-logo"/>
+       </div>
+);
+
+export default logo;

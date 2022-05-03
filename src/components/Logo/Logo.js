0ca009ca4f6ad5f09b6cecaaf898ad5ce42cfbda
IH-honnette <ihozohonnette12@gmail.com>
2022-05-03 22:05:39 +0200

    updated logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,6 +6,11 @@ import classes from './Logo.module.css';
 const logo =(props)=>(
        <div className={classes.Logo}>
            <img src={burgerLog} alt="burger-logo"/>
+           
+
+
+
+           
        </div>
 );
 

33a10e8f0cddc1f58a1e17bda774bbb5efbd78d6
IH-honnette <ihozohonnette12@gmail.com>
2022-05-04 22:00:19 +0200

    Toolbar responsiveness adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/ToolBar/ToolBar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout =(props) =>(
     <Aux>
    {/* <div>Toolbar, SideBar, Backdrop</div> */}
    <Toolbar/>
+   <SideDrawer />
    <main className={classes.Content}>
        {props.children}
    </main>

d96d3930c397c01d1ea32da40095422ff3328b72
IH-honnette <ihozohonnette12@gmail.com>
2022-05-04 11:16:09 +0200

    Added a sideDrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+
+const sidedrawer =(props)=> {
+// 
+
+        return (
+            
+            <div className={classes.SideDrawer}>
+                <Logo />
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        );
+
+}
+
+export default sidedrawer;

43952794f386eb57c14631b6d31d2f66efb414af
IH-honnette <ihozohonnette12@gmail.com>
2022-05-08 21:06:43 +0200

    updated the packages

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -20,6 +20,7 @@ const sidedrawer =(props)=> {
                      <Logo />
                 </div>
                
+        
                 <nav>
                     <NavigationItems />
                 </nav>
